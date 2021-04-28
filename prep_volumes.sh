27f0b8a5af19029efedd8ccd430c2651f4b3e494
Gary Schulte <garyschulte@gmail.com>
2021-04-28 10:04:25 -0700

    remove mining, add 3rd besu

--- prep_volumes.sh
+++ prep_volumes.sh
@@ -3,11 +3,12 @@
 # docker for mac does not auto create volume directories, prep here:
 rm -rf out 
 
-mkdir -p ./out/besu-1-chaindata/   \
- ./out/teku-bn-0-chaindata/        \
+mkdir -p ./out/besu-2-chaindata/   \
+ ./out/besu-1-chaindata/           \
  ./out/besu-0-chaindata/           \
+ ./out/teku-bn-0-chaindata/        \
  ./out/teku-bn-1-chaindata/        \
- ./out/teku-vc-1-chaindata/        \
  ./out/teku-vc-0-chaindata/        \
+ ./out/teku-vc-1-chaindata/        \
  ./out/teku-setup/
 
